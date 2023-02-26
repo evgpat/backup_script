97a881d4e8eacdd0bd51cb124d9aa502a1f0d55b
Evgeny Patochenko <e.a.patochenko@gmail.com>
2023-02-26 19:58:54 +0300

    add backup script

---
+++ backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+LOGFILE=error.log
+TMP_OUTPUT=tmp_output
+
+echo  >>$LOGFILE
+echo "====================================================="  >>$LOGFILE
+echo "$(date +'%d-%b-%Y %R')" >>$LOGFILE
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --backup_dir)
+      backup_dir="$2"
+      ;;
+    --algorithm)
+      algorithm="$2"
+      ;;
+    --output_file)
+      output_file="$2"
+      ;;
+  esac
+  shift
+done
+
+
+if [ -z "$algorithm" ]
+then
+  tar -c -f $TMP_OUTPUT -C $backup_dir $(ls -A $backup_dir) 2>>$LOGFILE
+else
+  tar -c -$algorithm -f $TMP_OUTPUT -C $backup_dir $(ls -A $backup_dir) 2>>$LOGFILE
+fi
+
+openssl aes-256-cbc -a -salt -pbkdf2 -in $TMP_OUTPUT -out $output_file
+
+rm $TMP_OUTPUT
+
+echo 'done' >>$LOGFILE
