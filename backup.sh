d32bdba6375c50c28c4690b30c9d99410c8f48ad
Evgeny Patochenko <e.a.patochenko@gmail.com>
2023-02-26 20:09:27 +0300

    update backup script

--- backup.sh
+++ backup.sh
@@ -7,6 +7,8 @@ echo  >>$LOGFILE
 echo "====================================================="  >>$LOGFILE
 echo "$(date +'%d-%b-%Y %R')" >>$LOGFILE
 
+
+# reading parameters
 while [ $# -gt 0 ]; do
   case "$1" in
     --backup_dir)
@@ -23,15 +25,18 @@ while [ $# -gt 0 ]; do
 done
 
 
-if [ -z "$algorithm" ]
+
+if [ -z "$algorithm" ] # case when compression algorithm is not defined
 then
   tar -c -f $TMP_OUTPUT -C $backup_dir $(ls -A $backup_dir) 2>>$LOGFILE
 else
   tar -c -$algorithm -f $TMP_OUTPUT -C $backup_dir $(ls -A $backup_dir) 2>>$LOGFILE
 fi
 
+# encrypt result
 openssl aes-256-cbc -a -salt -pbkdf2 -in $TMP_OUTPUT -out $output_file
 
+# remove temporary backup
 rm $TMP_OUTPUT
 
 echo 'done' >>$LOGFILE
